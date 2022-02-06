acc7075f3eb9ef41ffc10c40313f31ba9ce5d66b
TNsaturday <tnsaturday@gmail.com>
2022-02-07 02:37:19 +0300

    Add findfindOperatorWithBiggestPrecedence function

--- main.js
+++ main.js
@@ -7,24 +7,66 @@
 import { operatorPrecedence } from "./operatorPrecedence.js";
 import { calculate } from "./operations.js";
 
-console.log(tokenizeString("12-3*2"));
+const arrayExpression = tokenizeString("12-3+2/2");
+console.log(findOperatorWithBiggestPrecedence(arrayExpression));
 
-function tokenizeString(expression) {
+/**
+ * The algorithm that I came up with is such one:
+ * 1. Find the first operator with maximum precedence of the operatorPrecedence object.
+ * 2. Apply this operand for the numbers, that surround it (e.g. expressionArray[i-1] and expressionArray[i+1].
+ * 3. Remove said positions along with the operand itself from the array.
+ * 4. Put back the computation from the step 2.
+ * 5. Return new array and call function recursively.
+ * 6. Until there is only one item left, stop.
+ * @param expressionArray
+ */
+function evaluateExpression(expressionArray) {
+}
+
+/**
+ * Find operator with the biggest precedence
+ * @param array
+ * @returns {string}
+ */
+function findOperatorWithBiggestPrecedence(array) {
+    let operator = "";
+    let maxPrecedence = 0;
+    
+    for (let i = 0; i < array.length; i++) {
+        if (typeof array[i] === 'string') {
+            if (operatorPrecedence[array[i]] > maxPrecedence) {
+                maxPrecedence = operatorPrecedence[array[i]];
+                operator = array[i];
+            }
+        }
+    }
+
+    return operator;
+}
+
+/**
+ * Split string containing arithmetic expression into an array of operands and operators.
+ * Input must be a string pf an infix notation containing integer numbers and arithmetic operands.
+ * No parenthesis, variables or other operations support for the sake of simplicity.
+ * @param expressionString
+ * @returns {[]}
+ */
+function tokenizeString(expressionString) {
     const tokenArray = [];
 
     let token = "";
-    for (let i = 0; i < expression.length; i++) {
-        if (parseInt(expression[i])) {
-            token += expression[i];
+    for (let i = 0; i < expressionString.length; i++) {
+        if (parseInt(expressionString[i])) {
+            token += expressionString[i];
         } else {
-            tokenArray.push(token);
+            tokenArray.push(parseInt(token));
             token = "";
-            tokenArray.push(expression[i]);
+            tokenArray.push(expressionString[i]);
         }
     }
 
     if(token !== "") {
-        tokenArray.push(token);
+        tokenArray.push(parseInt(token));
     }
 
     return tokenArray;
