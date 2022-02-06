0a160dc573b19d002d2746f4f3aebd1a6c8255bc
TNsaturday <tnsaturday@gmail.com>
2022-02-07 01:52:07 +0300

    Add tokenizeString function which deals with valid strings that consists of numbers and arithmetic operatos in infix notation. No support for the parenthesis for now

--- main.js
+++ main.js
@@ -6,41 +6,26 @@
 
 import { operatorPrecedence } from "./operatorPrecedence.js";
 import { calculate } from "./operations.js";
-import { Stack } from "./Stack.js";
 
-// console.log(calculateExpression(5));
-console.log(calculateExpression("3+4/2-3"));
+console.log(tokenizeString("12-3*2"));
 
-function calculateExpression(stringExpression) {
-    if (typeof stringExpression !== "string") {
-        throw new TypeError(`String expected, got ${typeof stringExpression}`);
-    }
+function tokenizeString(expression) {
+    const tokenArray = [];
 
-    const numbersStack = new Stack();
-    const operationsStack = new Stack();
-
-    // Let's start with the assumption, that our input string consists of single digit numbers and binary operators
-    for (let i = 0; i < stringExpression.length; i++) {
-        if (parseInt(stringExpression[i])) {
-            numbersStack.push(parseInt(stringExpression[i]));
+    let token = "";
+    for (let i = 0; i < expression.length; i++) {
+        if (parseInt(expression[i])) {
+            token += expression[i];
         } else {
-            if (operationsStack.isEmpty()) {
-                operationsStack.push(stringExpression[i]);
-            } else {
-                if (operatorPrecedence[operationsStack.peek()] <= operatorPrecedence[stringExpression[i]]) {
-                    operationsStack.push(stringExpression[i]);
-                } else {
-                    // If the next operator has higher power than the previous one, take the
-                    // last two numbers from the numbers stack and execute the previous operation on them
-                    const number2 = numbersStack.pop();
-                    const number1 = numbersStack.pop();
-                    const operator = operationsStack.pop();
-                    numbersStack.push(calculate[operator](number1, number2));
-                    operationsStack.push(stringExpression[i]);
-                }
-            }
+            tokenArray.push(token);
+            token = "";
+            tokenArray.push(expression[i]);
         }
     }
 
-    return {numbersStack, operationsStack};
+    if(token !== "") {
+        tokenArray.push(token);
+    }
+
+    return tokenArray;
 }
