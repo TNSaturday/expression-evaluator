d675ba286d8ed3fd998c55ca39e8e090d01b7ad9
TNsaturday <tnsaturday@gmail.com>
2022-02-06 05:14:24 +0300

    Add operator precedence object and an empty Stack file

---
+++ main.js
@@ -0,0 +1,8 @@
+// Написать функцию вычисления простых выражений
+// Example:
+// bar("1+2*3") -> 7
+// bar("4/2*4-3") -> 5
+// bar("12-3") -> 9
+import { operatorPrecedence} from "./operator-precedence.js";
+
+console.log(operatorPrecedence);
---
+++ operator-precedence.js
@@ -0,0 +1,6 @@
+export const operatorPrecedence = {
+    "+": 1,
+    "-": 1,
+    "*": 2,
+    "/": 2
+};
